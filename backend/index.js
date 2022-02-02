2baf231bbef99356a754a0ef24c261db96b8261d
Ross Keeley <ross.keeley@sky.uk>
2022-02-02 15:20:23 +0000

    Added main package.json and simple express app

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,9 @@
-// hello world test
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});

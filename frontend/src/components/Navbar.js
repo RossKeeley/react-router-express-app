703860c283208d9274c1cc13f994ab2d554b535b
Ross Keeley <ross.keeley@sky.uk>
2022-02-02 18:07:09 +0000

    Links and styling for navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import "../styles/navbar.css";
 
 const Navbar = () => {
 
   return (
     <div className="navbar">
       <div className="navbar__logo">
-        <img src="#" alt="" />
-        <h1>GROW</h1>
+        <img className="navbar__logo-image" src="#" alt="" />
+        <h1 className="navbar__logo-title">
+          <Link className="navbar__logo-title-link" to="/">grow</Link>
+        </h1>
       </div>
-      <div className="navbar__searchbar">
-        <input type="text" />
+      <div className="navbar__searchbar-container">
+        <input className="navbar__searchbar" type="text" />
       </div>
       <ul className="navbar__list">
-        <li className="navbar__list-item"><Link to="about">about</Link></li>
-        <li className="navbar__list-item"><Link to="contact">contact</Link></li>
+        <li className="navbar__list-item">
+          <Link className="navbar__list-item-link" to="about">about</Link>
+        </li>
+        <li className="navbar__list-item">
+          <Link className="navbar__list-item-link" to="contact">contact</Link>
+        </li>
         {/* <li><Link to="profile">profile</Link></li> */}
       </ul>
     </div>
